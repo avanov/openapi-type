ee83bd4618b3ac0b5435448b6aa29df60bff2074
Maxim Avanov <601955+avanov@users.noreply.github.com>
2020-12-29 03:06:42 +0000

    Release 0.0.3 (#2)
    * fix manifest
    
    * update readme
    
    * new spec test and refactor
    
    * Version bump

--- tests/paths.py
+++ tests/paths.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 TESTS_ROOT             = Path(__file__).parent.absolute()
 SPEC_EXAMPLES_DIR      = TESTS_ROOT.parent / "specification" / "examples" / "v3.0"
+CUSTOM_EXAMPLES_DIR    = TESTS_ROOT / "custom_examples"
 
 SPECS = [
     (x, SPEC_EXAMPLES_DIR / f"{x}.json") for x in [
@@ -10,5 +11,10 @@ SPECS = [
         'api-with-examples',
         'callback-example',
         'link-example',
-        'uspto'
-]]
+        'uspto',
+    ]
+] + [
+    (x, CUSTOM_EXAMPLES_DIR / f"{x}.json") for x in [
+        "one"
+    ]
+]
