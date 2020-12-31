17ddbf0bf680982a4e3e0d04da21d0d957dc8f41
Maxim Avanov <601955+avanov@users.noreply.github.com>
2020-12-31 17:45:25 +0000

    Release 0.0.6 (#5)
    * Parse Media Type with charset
    
    * Release 0.0.6
    
    * fix tests

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with (here / 'README.rst').open() as f:
 # ----------------------------
 
 setup(name='openapi-type',
-      version='0.0.5',
+      version='0.0.6',
       description='OpenAPI Type',
       long_description=README,
       classifiers=[
