5109b4d77ecff7485dea54feebd92470232d42cb
Maxim Avanov <maxim.avanov@gmail.com>
2021-01-11 17:40:57 +0000

    Release 0.0.17

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with (here / 'README.rst').open() as f:
 # ----------------------------
 
 setup(name='openapi-type',
-      version='0.0.16',
+      version='0.0.17',
       description='OpenAPI Type',
       long_description=README,
       classifiers=[
